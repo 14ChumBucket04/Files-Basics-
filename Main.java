e33e4c1ce616841f6092829e001c8c64afa19f10
Plankton <123856162+14ChumBucket04@users.noreply.github.com>
2024-04-30 13:38:53 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,44 @@
+import java.io.*;
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		File file = new File("C:\\Coding\\Java\\Files\\src\\Secret.txt");
+		
+		if(file.exists()) 
+		{
+			System.out.println("This File Exits!");
+		}
+		else
+		{
+			System.out.println("File Does not Exits!");
+		}
+	/*try 
+	{
+		FileWriter writer = new FileWriter("C:\\Coding\\Java\\Files\\src\\Secret.txt");
+		writer.write("Hello World \n Its how the file writer function works and i used 'write' to add text");
+		writer.append("\n(File Writer)");
+		writer.close();
+	}
+	catch (IOException e)
+	{
+		e.printStackTrace();
+	}*/
+	try {
+		FileReader reader = new FileReader("C:\\Coding\\Java\\Files\\src\\Secret.txt");
+		int data = reader.read();
+		while(data != -1)
+		{
+			System.out.print((char)data);
+			data = reader.read();
+		}
+		reader.close();
+	}
+	catch (IOException e)
+	{
+		e.printStackTrace();
+	}
+	
+}
+
+}	
